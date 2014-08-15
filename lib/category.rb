b978794b1df7b1917b0cb78ea8d80048fe645df3
Erica Gordon & Justin Pacubas <student@epicodus.com>
2014-08-15 11:45:27 -0700

    Create method to save category to the database.

--- lib/category.rb
+++ lib/category.rb
@@ -6,4 +6,23 @@ class Category
     @name = attributes['name']
     @budget = attributes['budget'].to_f
   end
+
+  def ==(another_category)
+    self.id == another_category.id && self.name == another_category.name && self.budget == another_category.budget
+  end
+
+  def save
+    results = DB.exec("INSERT INTO categories (name, budget) VALUES ('#{name}', #{budget}) RETURNING id;")
+    @id = results.first['id'].to_i
+  end
+
+  def self.all
+    results = DB.exec("SELECT * FROM categories;")
+    categories_list = []
+    results.each do |result|
+      new_category = Category.new(result)
+      categories_list << new_category
+    end
+    categories_list
+  end
 end
--- spec/category_spec.rb
+++ spec/category_spec.rb
@@ -5,4 +5,10 @@ describe Category do
     test_category = Category.new({'name' => 'Home', 'budget' => 300.00})
     expect(test_category).to be_an_instance_of Category
   end
+
+  it 'saves the category to the database' do
+    test_category = Category.new({'name' => 'Home', 'budget' => 300.00})
+    test_category.save
+    expect(Category.all).to eq [test_category]
+  end
 end
